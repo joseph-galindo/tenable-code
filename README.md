c9adad9b3f043f0c43a8bfbb052160a2ad75fbca
Joseph Galindo <jgalindo@cdmprinceton.com>
2016-12-11 01:59:46 -0500

    expressJS server runs correctly, barebones file structure done

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # tenable-code
+
+__Build instructions__
+
+1. Install [node](http://nodejs.org) (the version used to build and run this code was node v4.3.0).   
+2. Clone this directory, and run `npm install` in your terminal of choice to install the needed node modules (mainly express and its middleware).  
+3. Run `npm start` from the terminal to begin the expressJS server that runs the backend endpoints, as well as serves the frontend HTML.  
+4. In your browser of choice, navigate to `http://localhost:3000` to see the content. Possible links are:  
+   * `http://localhost:3000`  
+   * `http://localhost:3000/req1`  
+   * `http://localhost:3000/req2`  
+   * `http://localhost:3000/req3`  
+5. When finished, end the running node script from step 3 in your terminal of choice.  
---
+++ server.js
@@ -0,0 +1,78 @@
+//setup requires
+const express = require("express");
+const body_parser = require("body-parser");
+const port = 3000;
+
+//setup express server
+var app = express();
+
+//parse incoming requests bodies
+app.use(body_parser.json()); // application/json
+app.use(body_parser.urlencoded({extended: true})); // application/x-www-form-urlencoded
+
+//set up the HTML content to be served
+app.use(express.static(__dirname));
+
+//-----BACKEND CODE BEGINS-----//
+
+//set up a router for incoming requests
+var router = express.Router();
+
+router.get('/', function(theReq, theRes) {
+	theRes.json({
+		message: 'root message'
+	})
+});
+
+router.get('/request', function(theReq, theRes) {
+
+	if(theReq.query.host) {
+
+		if(isFinite(theReq.query.host)) {
+			theRes.json({
+				message: 'Successful download request!'
+			});
+		} else {
+			theRes.json({
+				message: 'Download request failed. \'Host\' parameter specified, but was not a number.'
+			});
+		}
+
+	} else {
+		theRes.json({
+			message: 'Download request failed. \'Host\' parameter was not specified.'
+		});
+	}
+});
+
+//incoming requests on http://[ourserver]/download/** will be directed to our router above.
+//this use rule is declared here after the router is established
+app.use('/download', router);
+
+//-----BACKEND CODE ENDS-----//
+
+//-----FRONTEND CODE BEGINS-----//
+
+//This code handles intercepting URL requests and forwarding along the correct HTML files to be displayed
+//in the user's browser
+
+app.get('/', function(theReq, theRes) {
+	theRes.sendFile(__dirname + '/index.html');
+});
+
+app.get('/req1', function(theReq, theRes) {
+	theRes.sendFile(__dirname + '/req1/index.html');
+});
+
+app.get('/req2', function(theReq, theRes) {
+	theRes.sendFile(__dirname + '/req2/index.html');
+});
+
+app.get('/req3', function(theReq, theRes) {
+	theRes.sendFile(__dirname + '/req3/index.html');
+});
+
+//-----FRONTEND CODE ENDS-----//
+
+//set the server to listen for incoming requests
+app.listen(port);
